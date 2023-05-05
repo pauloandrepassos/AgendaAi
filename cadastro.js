e3d4f3a1a36ea2a1a691d4935970b1cc5c4cb53c
Carlos Michael <carlosmaykesilva@gmail.com>
2023-05-05 14:37:15 -0300

    Telas de login e cadastro
    telas praticamente prontas de login e cadastro

---
+++ cadastro.js
@@ -0,0 +1,24 @@
+const togglePassword = document.querySelector('#togglePassword1');
+        const password = document.querySelector('#password');
+
+            togglePassword.addEventListener('click', function (e) {
+                // toggle the type attribute
+                const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+                password.setAttribute("type", type);
+                this.classList.toggle('fa-eye');
+                this.classList.toggle('fa-eye-slash');
+
+
+                // toggle the eye slash icon
+            });
+
+const togglePassword2 = document.querySelector('#togglePassword2');
+        const password2 = document.querySelector('#confirm-password');
+
+            togglePassword2.addEventListener('click', function (e) {
+
+                const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+                password2.setAttribute("type", type);
+                this.classList.toggle('fa-eye');
+                this.classList.toggle('fa-eye-slash');
+            });

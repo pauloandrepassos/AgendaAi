4f6ca32a6eb210c76d19104631fc683336308006
Carlos Michael <carlosmaykesilva@gmail.com>
2023-06-07 13:45:03 -0300

    Telas de agendamento e históricos
    Tela de agendamento e as telas de histórico de compras (usuário) e de vendas (gerente) sofreram mudança de design com melhoria de eficiência

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-const cardMinimizado = document.getElementById('cardMinimizado');
-const cardMax = document.getElementById('cardMax');
+cardAberto = false;
 
-let cardAberto = false;
+if (document.title.includes("Agendamentos")) {
+  const pedidoMinimizado = document.querySelectorAll('.pedido-minimizado');
+  const pedidoMax = document.querySelectorAll('.pedido-maximizado');
 
-cardMinimizado.addEventListener('click', function() {
-  if (cardAberto) {
-    cardMax.style.display = 'none';
-    cardAberto = false;
-  } else {
-    cardMax.style.display = 'block';
-    cardAberto = true;
+  for (let i = 0; i < pedidoMinimizado.length; i++) {
+    pedidoMinimizado[i].addEventListener('click', function() {
+      if (cardAberto) {
+        pedidoMax[i].style.display = 'none';
+        cardAberto = false;
+      } else {
+        pedidoMax[i].style.display = 'block';
+        cardAberto = true;
+      }
+    });
   }
-});
+} else if (document.title.includes("Histórico")) {
+  const pedidoMinimizado = document.querySelectorAll('.pedido-minimizado');
+  const pedidoMax = document.querySelectorAll('.pedido-maximizado');
+
+  for (let i = 0; i < pedidoMinimizado.length; i++) {
+    pedidoMinimizado[i].addEventListener('click', function() {
+      if (cardAberto) {
+        pedidoMax[i].style.display = 'none';
+        cardAberto = false;
+      } else {
+        pedidoMax[i].style.display = 'block';
+        cardAberto = true;
+      }
+    });
+  }
+} else if (document.title.includes("Avaliação")) {
+  const cardMinimizado = document.getElementById('cardMinimizado');
+  const cardMax = document.getElementById('cardMax');
+
+  cardMinimizado.addEventListener('click', function() {
+    if (cardAberto) {
+      cardMax.style.display = 'none';
+      cardAberto = false;
+    } else {
+      cardMax.style.display = 'block';
+      cardAberto = true;
+    }
+  });
+  
+}
+
+
+
+
+
+
